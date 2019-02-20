34e7ef2c23b380f568c4547267471d3ebe1ed362
Sarah <sarahseewhy@gmail.com>
2019-02-20 15:31:45 +0000

    spike(retrieve-event): Extract logic to smaller methods

--- core/main.py
+++ core/main.py
@@ -14,61 +14,92 @@ SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 
 def main():
-    # Shows basic usage of the Google Calendar API.
-    # Prints the start and name of the next 10 events on the user's calendar.
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('env/credentials.json', SCOPES)
 
-            # Tutorial says to use flow.run_local_server() but this also works.
-            creds = flow.run_console()
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+    credentials = configure_app_authorization()
+    calendar = build('calendar', 'v3', credentials=credentials)
+    time_max, time_min, week_ago = calculate_timeframe()
+    calendar_items = retrieve_calendar_items_from(calendar, time_max, time_min)
+    events = calendar_items.get('items', [])
+    event_types = extract_type_from(events)
 
-    service = build('calendar', 'v3', credentials=creds)
+    output = generate_output(event_types)
 
-    today = datetime.today()
-    week_ago = today - relativedelta(weeks=1)
-    time_max = today.isoformat('T') + "Z"
-    time_min = week_ago.isoformat('T') + "Z"
+    display_output(output, week_ago)
 
-    # Call the Calendar API
-    events_result = service.events().list(calendarId='primary', timeMin=time_min, timeMax=time_max,
-                                          maxResults=50, singleEvents=True,
-                                          orderBy='startTime').execute()
-    events = events_result.get('items', [])
 
+def display_output(output, week_ago):
+    print('Team events since %s' % week_ago.date())
+    print(output)
+
+
+def generate_output(event_types):
+    types = Counter(event_types).keys()
+    frequency = Counter(event_types).values()
+    return dict(zip(types, frequency))
+
+
+def extract_type_from(events):
     event_types = []
-
     if not events:
         print('No upcoming events found.')
     for event in events:
         # retrieve the summary, currently the only thing I'm interested in
         summary = event["summary"]
-
         # extract the event from the summary
         event_type = extract_event_type_from(summary)
-
         # add types to array of types
         event_types.append(event_type)
         # output event types from the given time period
+    return event_types
 
-    types = Counter(event_types).keys()  # equals to list(set(words))
-    frequency = Counter(event_types).values()  # counts the elements' frequency
 
-    print('Team events since %s' % week_ago.date())
-    print(dict(zip(types, frequency)))
+def retrieve_calendar_items_from(service, time_max, time_min):
+    events_result = service.events().list(calendarId='primary', timeMin=time_min, timeMax=time_max,
+                                          maxResults=50, singleEvents=True,
+                                          orderBy='startTime').execute()
+    return events_result
+
+
+def calculate_timeframe():
+    today = datetime.today()
+    week_ago = today - relativedelta(weeks=1)
+    time_max = today.isoformat('T') + "Z"
+    time_min = week_ago.isoformat('T') + "Z"
+    return time_max, time_min, week_ago
+
+
+def configure_app_authorization():
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    credentials = load_token()
+    # If there are no (valid) credentials available, let the user log in.
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            credentials = authorize_credentials()
+        with open('token.pickle', 'wb') as token:
+            save_credentials(credentials, token)
+    return credentials
+
+
+def save_credentials(credentials, token):
+    pickle.dump(credentials, token)
+
+
+def authorize_credentials():
+    flow = InstalledAppFlow.from_client_secrets_file('env/credentials.json', SCOPES)
+    credentials = flow.run_console()
+    return credentials
+
+
+def load_token():
+    credentials = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            credentials = pickle.load(token)
+    return credentials
 
 
 def extract_event_type_from(summary):
